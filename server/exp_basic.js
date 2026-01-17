9c22fca3462b430e20c88697f4c8802836c2fd12
niteen-singh <viperxoffic@gmail.com>
2026-01-17 19:58:13 +0530

    starting Express

---
+++ server/exp_basic.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("home Page");    
+});
+
+app.listen(8000, () => console.log("server Started"));
