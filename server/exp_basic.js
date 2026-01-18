314bbe9e9641929adcef5118f1ea1faec04a237b
niteen-singh <viperxoffic@gmail.com>
2026-01-18 14:07:17 +0530

    Query Handling

--- server/exp_basic.js
+++ server/exp_basic.js
@@ -6,4 +6,8 @@ app.get("/", (req, res) => {
     res.send("home Page");    
 });
 
+app.get("/about", (req, res) => {
+    res.send(`Hi ${req.query.name} your ID is ${req.query.id}`);
+});
+
 app.listen(8000, () => console.log("server Started"));
