eb1686396c1ea5f5e2604925932702ea896a9685
niteensingh <viperxoffic@gmail.com>
2026-02-10 22:43:04 +0530

    Add URL_shortener project

---
+++ URL_shortener/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { nanoid } from "nanoid";
+
+const app = express();
+const port = 8000;
+
+//const code = nanoid(6);
+//console.log(code);
+
+app.post("/url", (req, res) => {
+
+})
+
+app.get("/:id", (req, res) => {
+
+})
+
+app.get("/url/analytics/:id", (req, res) => {
+
+})
+
+app.listen(port, () => { console.log(`server started on port ${port}`)});
