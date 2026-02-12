8cb927d1661016407d0519f75afa538b4c745bd1
niteen-singh <viperxoffic@gmail.com>
2026-02-12 20:14:52 +0530

    working on frontend

---
+++ URL_shortener/backend/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+require("dotenv").config();
+const { nanoid } = require("nanoid");
+const pool = require("./db")
+
+const app = express();
+const port = process.env.PORT;
+app.use(express.json()); 
+
+//const code = nanoid(6);
+//console.log(code);
+
+app.post("/url", async (req, res) => {
+    try{
+        const url = req.body.url;
+        const short_url = nanoid(6);
+        const query = `INSERT INTO urls(short_code, orignal_url) VALUES($1, $2) RETURNING *;`;
+        const values = [short_url, url];  
+        const result = await pool.query(query, values);
+        res.status(201).json({
+            result: result.rows[0]});
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+})
+
+app.get("/url/analytics/:id", async (req, res) => {
+    try{
+        const short_url = req.params.id;
+        const query = `SELECT analytics FROM urls WHERE short_code = $1;`;
+        const result = await pool.query(query, [short_url]);
+        res.status(200).json({
+            result: result.rows[0]});
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+})
+
+app.get("/:id", async (req, res) => {
+    try{
+        const short_url = req.params.id;
+        const query = `SELECT orignal_url FROM urls WHERE short_code=$1;`;
+        const ins_query = `UPDATE urls
+            SET analytics = analytics + 1
+            WHERE short_code = $1`;
+        await pool.query(ins_query, [short_url]);
+        const result = await pool.query(query, [short_url]);
+        res.redirect(result.rows[0].orignal_url);
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Server error");
+    } 
+})
+
+app.listen(port, () => { console.log(`server started on port ${port}`)});
